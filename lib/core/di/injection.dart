1c673f2f98a86116ecaf9c834ce7a5fbe70a9357
Hemko364 <hermann@lawolapp.com>
2025-11-09 22:03:28 +0100

    feat: add Firebase Analytics tracking for authentication events
    - Integrated Firebase Analytics into dependency injection and auth service
    - Added analytics logging for login (email, Google, Apple), sign up, and logout events
    - Updated auth methods to track user authentication flows

--- lib/core/di/injection.dart
+++ lib/core/di/injection.dart
@@ -1,6 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 import '../../../data/firebase/auth_service.dart';
 
 final getIt = GetIt.instance;
@@ -9,11 +10,15 @@ void setupInjection() {
   // Firebase Auth
   getIt.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
 
+  // Firebase Analytics
+  getIt.registerLazySingleton<FirebaseAnalytics>(() => FirebaseAnalytics.instance);
+
   // Secure Storage
   getIt.registerLazySingleton<FlutterSecureStorage>(() => const FlutterSecureStorage());
 
   // Auth Service
   getIt.registerLazySingleton<FirebaseAuthService>(() => FirebaseAuthService(
         getIt<FirebaseAuth>(),
+        getIt<FirebaseAnalytics>(),
       ));
 }
