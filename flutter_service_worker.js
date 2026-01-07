8ac1dbabf58befdbe2f9b22b1fc5d14c85ed7cbc
nacroalfa-gif <nacroalfa-gif@users.noreply.github.com>
2026-01-07 23:24:09 +0000

    deploy: 66556ba5767c830d5bd1df31dcfa1a13574138bf

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "canvaskit/canvaskit.js.symbols": "a3c9f77715b642d0437d9c275caba91e",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "93540e65e51cca75b1842e096ed0a02b",
+"flutter_bootstrap.js": "e1a7327231c497b43af26c3f9cfd283a",
 "version.json": "7740683ea83fa905edb01d38c5a23d22",
 "main.dart.js": "55702a6a12ea51e719a363bc96b35f2b"};
 // The application shell files that are downloaded before a service worker can
